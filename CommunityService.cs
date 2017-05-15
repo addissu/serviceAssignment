df9ff60207ccfdd3250114f019ad805b2226f6d0
addissu <atamru05@gmail.com>
2017-05-14 22:02:12 -0700

    Add files via upload

---
+++ CommunityService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "CommunityService" in code, svc and config file together.
+public class CommunityService : ICommunityService
+{
+    Community_AssistEntities db = new Community_AssistEntities();
+
+    public List<PersonInfo> GetInfo()
+    {
+        var per = from b in db.People
+                  select new
+                  {
+                      b.PersonLastName,
+                      b.PersonFirstName,
+                      b.PersonEmail,
+                      b.PersonPassWord,
+                      b.PersonAddresses,
+
+                  };
+        List<PersonInfo> info = new List<PersonInfo>();
+        foreach (Person pe in per)
+        {
+            PersonInfo p1 = new PersonInfo();
+            p1.lastName = pe.PersonLastName;
+            p1.firstName = pe.PersonFirstName;
+            p1.email = pe.PersonEmail;
+
+            PersonInfo.Add(p1);
+
+        }
+        return info;
+        
+    }
+    
+   
+
+    public PersonInfo GetInfo(string infoo)
+    {
+        var gin = (from b in db.People from a in b.PersonKey where a.person.Equals(infoo) select b);
+        return gin;
+    }
+
+    public int Loginin(string user, string password)
+    {
+        int key = 0;
+        int result = db.usp_Login(user, password);
+        if (result != -1)
+        {
+            var userKey = (from k in db.People where k.PersonEmail.Equals(user) select k.PersonKey).FirstOrDefault();
+            key = (int)userKey;
+        }
+        return key;
+    }
+
+    public bool RegisterUser(Person u)
+    {
+        bool result = true;
+        int use = db.usp_Register (u.PersonLastName, u.PersonFirstName, u.PersonEmail, u.PersonPassWord, u.PersonAddresses);
+
+        return result;
+
+    }
+}
---
+++ ICommunityService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ICommunityService" in both code and config file together.
+[ServiceContract]
+public interface ICommunityService
+{
+    [OperationContract]
+    int Loginin(string user, string password);
+
+    [OperationContract]
+    bool RegisterUser(Person u);
+
+    [OperationContract]
+    List<PersonInfo> GetInfo();
+
+    [OperationContract]
+
+    PersonInfo GetInfo(string lastName, string firstName, string email, string password, string address);
+}
+
+[DataContract]
+public class PersonInfo
+{
+    [DataMember]
+    public string lastName { get; set; }
+    [DataMember]
+    public string firstName { get; set; }
+    [DataMember]
+    public string email { get; set; }
+    [DataMember]
+    public string password { get; set; }
+    [DataMember]
+    public string apartmentNumber { get; set; }
+
+    internal static void Add(PersonInfo p1)
+    {
+        throw new NotImplementedException();
+    }
+
+    //[DataMember]
+    // public string street { get; set; }
+    [DataMember]
+    public string city { get; set; }
+    [DataMember]
+    public string state { get; set; }
+    [DataMember]
+    public string zipCode { get; set; }
+    [DataMember]
+    public string homePhone { get; set; }
+    [DataMember]
+    public string workPhone { get; set; }
+   
+
+}
